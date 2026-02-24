Fixed sign in isoplanatic angle

src/optical_turbulence/flat_earth.py
@@ -239,7 +239,7 @@ def isonoplanatic_angle_UL(wavelength: real_t, \
     mu_frac = (altitude - lct_altitude) / link_len
 
     mu1u = integrate.simpson(ris_model(altitude) * 
-        np.pow( Theta - neg_Theta * mu_frac , 5/3 ),
+        np.pow( Theta + neg_Theta * mu_frac , 5/3 ),
         altitude
     )
 

src/optical_turbulence/round_earth.py
@@ -322,7 +322,7 @@ def isonoplanatic_angle_UL(wavelength: real_t, \
     mu_frac = (distance - lct_distance) / link_distance
 
     mu1u = integrate.simpson(ris_model(altitude) * 
-        np.pow( Theta - neg_Theta * mu_frac , 5/3 ),
+        np.pow( Theta + neg_Theta * mu_frac , 5/3 ),
         distance
     )
 