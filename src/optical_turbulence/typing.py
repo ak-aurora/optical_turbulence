bfe6b865b1d5e84a80248b9f750337b895f5b436
Agustin Gonzalez <agustin.gonzalez@fresnel.fr>
2026-04-26 13:00:26 +0200

    Formatting improvements

--- src/optical_turbulence/typing.py
+++ src/optical_turbulence/typing.py
@@ -2,15 +2,15 @@ import numpy as np
 import numpy.typing as npt
 
 # ----------------- TYPE DEFINITIONS ----------------- #
-#region Type Definitions
+# region Type Definitions
 
-real_t = int | float | np.float64 | np.int_ #| np.float128
+real_t = int | float | np.float64 | np.int_  # | np.float128
 """Real scalar values"""
 
-real_array_t = npt.NDArray[np.float64] | npt.NDArray[np.int_] #| npt.NDArray[np.float128]
+real_array_t = (
+    npt.NDArray[np.float64] | npt.NDArray[np.int_]
+)  # | npt.NDArray[np.float128]
 """Array of floats or integers"""
 
 
-#endregion Type Definitions
-
-
+# endregion Type Definitions
