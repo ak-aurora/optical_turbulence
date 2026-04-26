b6e1ff2f003fd82b6dff2523851940a8f10d5d86
Agustin Gonzalez <agustin.gonzalez@fresnel.fr>
2026-04-26 13:01:16 +0200

    Implemented new classes and earth models

--- src/optical_turbulence/classes.py
+++ src/optical_turbulence/classes.py
@@ -2,26 +2,42 @@
 Classes to facilitate the use of the library
 """
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 import warnings
 from .typing import real_t
-from .earth_models import EarthModel
-
-from typing import Dict
+from typing import Dict, Callable, TypedDict
+from .earth_models import EarthModelGeneric
 
 import numpy as np
+import numpy.typing as npt
 
+class OpticalBeamParameters(TypedDict):
+    beam_radius: real_t
+    pfront_radius: real_t
+    wavelength: real_t
+    link_distance: real_t
+    amplitude: real_t
+    wavenumber: real_t
+    refractive_param_transmitter: real_t
+    diffractive_param_transmitter: real_t
+    refractive_param_receiver: real_t
+    diffractive_param_receiver: real_t
+    Lambda: real_t
+    Theta: real_t
+    rx_spot_size: real_t
+    rx_pfront_radius: real_t
+    neg_Theta: real_t
 
 @dataclass
 class OpticalBeam:
     """Based on the Gaussian-Beam Wave Model detailed in [1]
-    
+
     [1] L. C. Andrews and M. Beason, Laser beam propagation in random media: new and advanced topics. Bellingham, Washington, USA: SPIE Press, 2023.
     """
 
     # ----------------- Fixed attributes ----------------- #
 
-    beam_radius : real_t
+    beam_radius: real_t
     """[m] : Effective beam radius (spot size) at which the field amplitude falls to 1/e"""
 
     pfront_radius: real_t
@@ -38,88 +54,87 @@ class OpticalBeam:
 
     # ----------------- Dynamic attributes ----------------- #
 
-    _params_changed: bool = True
-    _dic : dict | None = None
+    _params_changed: bool = field(init=False, default=True)
+    _dic: OpticalBeamParameters | None = field(init=False, default=None)
 
     # ----------------- At-run-time-calculated properties ----------------- #
 
     @property
-    def wavenumber(self):
+    def wavenumber(self) -> real_t:
         """[1/m] : 2 * pi / wavelength"""
 
         return 2 * np.pi / self.wavelength
 
-
     @property
-    def refractive_param_transmitter(self):
+    def refractive_param_transmitter(self) -> real_t:
         """[unitless] : (Theta_0) Non-dimensional refractive beam parameter at the transmitter."""
 
         value = 1 - self.link_distance / self.pfront_radius
         return value
 
-
     @property
-    def diffractive_param_transmitter(self):
+    def diffractive_param_transmitter(self) -> real_t:
         """[unitless] : (Lambda_0) Non-dimensional diffractive beam parameter at the transmitter."""
 
-        value = 2 * self.link_distance / ( self.wavenumber * np.pow(self.beam_radius, 2) )
+        value = 2 * self.link_distance / (self.wavenumber * np.pow(self.beam_radius, 2))
         return value
 
-
     @property
-    def refractive_param_receiver(self):
+    def refractive_param_receiver(self) -> real_t:
         """[unitless] : (Theta) Non-dimensional refractive beam parameter at the receiver."""
-        
-        denominator = np.pow(self.refractive_param_transmitter, 2) + np.pow(self.diffractive_param_transmitter, 2)
+
+        denominator = np.pow(self.refractive_param_transmitter, 2) + np.pow(
+            self.diffractive_param_transmitter, 2
+        )
 
         value = self.refractive_param_transmitter / denominator
         return value
 
-
     @property
-    def diffractive_param_receiver(self):
+    def diffractive_param_receiver(self) -> real_t:
         """[unitless] : (Lambda) Non-dimensional diffractive beam parameter at the receiver."""
 
-        denominator = np.pow(self.refractive_param_transmitter, 2) + np.pow(self.diffractive_param_transmitter, 2)
+        denominator = np.pow(self.refractive_param_transmitter, 2) + np.pow(
+            self.diffractive_param_transmitter, 2
+        )
 
         value = self.diffractive_param_transmitter / denominator
         return value
-    
+
     @property
-    def Lambda(self):
+    def Lambda(self) -> real_t:
         """[unitless] : (Lambda) Non-dimensional diffractive beam parameter at the receiver."""
 
         return self.diffractive_param_receiver
 
-
     @property
-    def Theta(self):
+    def Theta(self) -> real_t:
         """[unitless] : (Theta) Non-dimensional refractive beam parameter at the receiver."""
 
         return self.refractive_param_receiver
 
-
     @property
-    def rx_spot_size(self):
+    def rx_spot_size(self) -> real_t:
         """[m] : $W$ spot size (effective beam radius) of the beam at the receiver plane"""
 
-        in_sqrt = np.pow(self.refractive_param_transmitter, 2) + np.pow(self.diffractive_param_transmitter, 2)
+        in_sqrt = np.pow(self.refractive_param_transmitter, 2) + np.pow(
+            self.diffractive_param_transmitter, 2
+        )
 
         value = self.beam_radius * np.sqrt(in_sqrt)
         return value
 
     @property
-    def rx_pfront_radius(self):
+    def rx_pfront_radius(self) -> real_t:
         """[m] : Phase front radius of curvature at the receiver."""
 
         value = self.link_distance / (self.Theta - 1)
         return value
-    
+
     @property
-    def neg_Theta(self):
+    def neg_Theta(self) -> real_t:
         """[unitless] : (overbar Theta) **Negated** non-dimensional refractive beam parameter at the receiver."""
         return 1 - self.Theta
-
 
     # ----------------- Methods ----------------- #
 
@@ -131,9 +146,8 @@ class OpticalBeam:
 
         super().__setattr__(name, value)
 
-    
-    def as_dict(self) -> Dict:
-        """ Return all properties and attributes of this class a dictionary
+    def as_dict(self) -> OpticalBeamParameters:
+        """Return all properties and attributes of this class a dictionary
         to be used in **kwargs.
 
         Returns
@@ -141,33 +155,146 @@ class OpticalBeam:
         """
 
         if self.link_distance <= 0:
-            warnings.warn(f"OpticalBeam instance has an invalid link distance.\
-                Link distance: {self.link_distance}")
+            warnings.warn(
+                f"OpticalBeam instance has an invalid link distance.\
+                Link distance: {self.link_distance}"
+            )
 
         if self._params_changed or self._dic is None:
-            self._dic = {prop: getattr(self, prop) for prop in dir(self) if not (prop.startswith('_') or callable(getattr(self, prop, None)))}
+            self._dic = OpticalBeamParameters(**{  # ty:ignore[missing-typed-dict-key]
+                prop: getattr(self, prop)
+                for prop in dir(self)
+                if not (prop.startswith("_") or callable(getattr(self, prop, None)))
+            })
             self._params_changed = False
 
         return self._dic
 
 
-@dataclass
-class LinkDescriptor:
-    """Descriptor for a sat-Earth link."""
-    # TODO: Altitudes w.r.t ground/sea level
 
-    satellite_altitude: real_t
-    """[m] : Altitude of the satellite."""
+class LinkDescriptionArrays(TypedDict):
+    altitude_array: npt.NDArray[np.float64]
+    link_array: npt.NDArray[np.float64]
 
-    lct_altitude: real_t
-    """[m] : Altitude of the Laser Communication Terminal/Optical Ground Station"""
+class LinkDescriptionManager():
+    """Manage the link, auto calculating relevant values in case of an update"""
 
-    zenith_angle: real_t
-    """[rad] : Zenith angle of the link."""
+    def __init__(
+            self,
+            satellite_altitude: real_t,
+            lct_altitude: real_t,
+            zenith_angle: real_t,
+            earth_model: EarthModelGeneric,
+            altitude_array_factory: Callable[[real_t, real_t], npt.NDArray[np.float64]]
+    ):
+        """Constructor
 
-    earth_model: EarthModel
-    """[n/a] : Earth model to use"""
+        Args:
+            satellite_altitude (real_t): satellite altitude [m]
+            lct_altitude (real_t): LCT/OGS altitude [m]
+            zenith_angle (real_t): zenith angle of the link [rad]
+            earth_model (EarthModelGeneric): earth model [n/a]
+            altitude_array_factory (Callable[[sat_altitude: real_t, \
+                lct_altitude: real_t], altitude_array: npt.NDArray[np.float64]]): \
+                callable to slice the atmosphere all values have to be in meters.
+        """
 
+        self.sat_altitude = satellite_altitude
+        self.lct_altitude = lct_altitude
+        self.zenith_angle = zenith_angle
+        self.earth_model = earth_model
+        self.altitude_array_factory = altitude_array_factory
+        
+        self._update_altitude_array()
+        self._update_link_array()
+
+        self._update_arrays = False
+
+    def __setattr__(self, name, value):
+        # We want to update the arrays if the link description changes
+        super().__setattr__(name, value)
+
+        if "link_array" not in name \
+            and "altitude_array" not in name\
+            and "update_arrays" not in name:
+            self._update_arrays = True
+
+    @property
+    def altitude_array(self) -> npt.NDArray[np.float64]:
+
+        if self._update_arrays:
+            self._update_altitude_array()
+            self._update_link_array()
+            self._update_arrays = False
+
+        return self._altitude_array
+
+    @altitude_array.setter
+    def altitude_array(self, _) -> None:
+        raise AttributeError("Do not set the value of the altitude array manually, update an attribute.")
+
+    @property
+    def link_array(self) -> npt.NDArray[np.float64]:
+
+        if self._update_arrays:
+            self._update_altitude_array()
+            self._update_link_array()
+            self._update_arrays = False
+
+        return self._link_array
+    
+    @link_array.setter
+    def link_array(self, _) -> None:
+        raise AttributeError("Do not set the value of the link array manually, update an attribute.")
+
+    def _update_altitude_array(self):
+        self._altitude_array = self.altitude_array_factory(
+            self.sat_altitude,
+            self.lct_altitude
+        )
+
+    def _update_link_array(self):
+        self._link_array = self.earth_model.altitude_to_link(
+            altitude_array=self._altitude_array,
+            sat_altitude=self.sat_altitude,
+            lct_altitude=self.lct_altitude,
+            zenith_angle=self.zenith_angle
+        )
+
+    def arrays_as_dict(self) -> LinkDescriptionArrays:
+        """Return the altitude and link arrays as a dictionary to be used in
+        kwargs.
+
+        Returns:
+            LinkDescriptionArrays: keys "altitude_array" and "link_array"
+        """
+
+        if self._update_arrays:
+            self._update_altitude_array()
+            self._update_link_array()
+            self._update_arrays = False
+
+        ret: LinkDescriptionArrays = {
+            "altitude_array": self._altitude_array,
+            "link_array": self._link_array
+        }
+
+        return ret
+    
+    def link_distance(self) -> real_t:
+        """Calculate the total link distance between the OGS and satellite.
+
+        Returns:
+            real_t: the link distance
+        """
+
+        distance = self.earth_model.calculate_total_link_distance(
+            sat_altitude=self.sat_altitude,
+            lct_altitude=self.lct_altitude,
+            zenith_angle=self.zenith_angle
+        )
+
+        return distance
 
 if __name__ == "__main__":
     a = OpticalBeam.rx_spot_size
--- src/optical_turbulence/earth_models.py
+++ src/optical_turbulence/earth_models.py
@@ -1,21 +1,25 @@
 from abc import ABC, abstractmethod
-from typing import Callable
 
 import numpy as np
 
 from .typing import real_t, real_array_t
 
-# - - - - - - - - - - - - -
+# ----------------- CONSTANTS ----------------- #
+
+EARTH_RADIUS = 6371e3 # [m]
+
+# ----------------- GENERIC MODEL ----------------- #
  
-class EarthModel(ABC):
+class EarthModelGeneric(ABC):
 
     @staticmethod
     @abstractmethod
-    def altitude_to_link(altitude_array: real_array_t,
+    def altitude_to_link[T: real_t | real_array_t](
+                         altitude_array: T,
                          sat_altitude: real_t, 
                          lct_altitude: real_t, 
-                         zenith_angle: real_t
-                         ) -> real_array_t:
+                         zenith_angle: real_t,
+                         **_) -> T:
         """From an array with altitudes from sea level calculate the corresponding array with \
             link distances (distance from the OGS to that point for each altitude). 
 
@@ -34,11 +38,12 @@ class EarthModel(ABC):
 
     @staticmethod
     @abstractmethod
-    def link_to_altitude(link_array: real_array_t,
+    def link_to_altitude[T: real_t | real_array_t](
+                         link_array: T,
                          sat_altitude: real_t, 
                          lct_altitude: real_t, 
-                         zenith_angle: real_t
-                         ) -> real_array_t:
+                         zenith_angle: real_t,
+                         **_) -> T:
         """From an array with link distances calculate the corresponding array with altitudes \
         (altitude from sea level for each corresponding link distance). 
 
@@ -59,7 +64,8 @@ class EarthModel(ABC):
     @abstractmethod
     def calculate_total_link_distance(sat_altitude: real_t,
                                       lct_altitude: real_t,
-                                      zenith_angle: real_t) -> real_t:
+                                      zenith_angle: real_t,
+                                      **_) -> real_t:
         """Calculate the total link distance between LCT/OGS and satellite.
 
         Args:
@@ -74,32 +80,196 @@ class EarthModel(ABC):
         pass
 
 
+# ----------------- MODEL IMPLEMENTATIONS ----------------- #
 
-class FlatEarthModel(EarthModel):
-
-    def __init__(self) -> None:
-        super().__init__()
-
-    @staticmethod
-    def altitude_to_link(altitude_array: real_array_t,
-                         sat_altitude: real_t,
-                         lct_altitude: real_t,
-                         zenith_angle: real_t) -> real_array_t:
-        
-        
-        return np.array(2)
-
-
-
-class RoundEarthModel(EarthModel):
+class FlatEarthModel(EarthModelGeneric):
 
     def __init__(self) -> None:
         super().__init__()
 
     @staticmethod
-    def altitude_to_link(altitude_array: real_array_t,
-                         sat_altitude: real_t,
-                         lct_altitude: real_t,
-                         zenith_angle: real_t) -> real_array_t:
+    def altitude_to_link[T: real_t | real_array_t](
+                         altitude_array: T,
+                         sat_altitude: real_t, 
+                         lct_altitude: real_t, 
+                         zenith_angle: real_t,
+                         **_) -> T:
+        """From an array with altitudes from sea level calculate the corresponding array with \
+            link distances (distance from the OGS to that point for each altitude). 
+
+        Args:
+            altitude_array (real_array_t): array with altitudes from lct_altitude to sat_altitude [m].
+            sat_altitude (real_t): satellite altitude [m].
+            lct_altitude (real_t): laser communication terminal/optical ground station altitude [m].
+            zenith_angle (real_t): zenith angle of the link [rad].
+
+        Returns:
+            real_array_t: array with the corresponding link distances for the given altitudes [m].
+        """
         
-        return np.array(2)
+        # ensure we are below the satellite altitude
+        if np.max(altitude_array) > sat_altitude:
+            raise ValueError(f"Altitude {np.max(altitude_array):.2e} is greater than the satellite altitude ({sat_altitude:.2e})")
+        
+        if np.min(altitude_array) < lct_altitude:
+            raise ValueError(f"Altitude {np.min(altitude_array):.2e} is lower than LCT altitude ({lct_altitude:.2e})")
+
+        link_array = (altitude_array - lct_altitude) / np.cos(zenith_angle)  # ty:ignore[unsupported-operator]
+
+        return link_array
+
+    @staticmethod
+    def link_to_altitude[T: real_t | real_array_t](
+                         link_array: T,
+                         sat_altitude: real_t, 
+                         lct_altitude: real_t, 
+                         zenith_angle: real_t,
+                         **_) -> T:
+        """From an array with link distances calculate the corresponding array with altitudes \
+        (altitude from sea level for each corresponding link distance). 
+
+        Args:
+            link_array (real_array_t): array with link distances from the LCT to the satellite [m].
+            sat_altitude (real_t): satellite altitude [m].
+            lct_altitude (real_t): laser communication terminal/optical ground station altitude [m].
+            zenith_angle (real_t): zenith angle of the link [rad].
+
+        Returns:
+            real_array_t: array with the corresponding altitudes for the given link distances [m].
+        """
+
+        altitude_array = link_array * np.cos(zenith_angle) + lct_altitude
+
+        if (np.max(altitude_array) > sat_altitude):
+            raise ValueError(f"Altitude {np.max(altitude_array):.2e} is greater than the satellite altitude ({sat_altitude:.2e})")
+        
+        if np.min(altitude_array) < lct_altitude:
+            raise ValueError(f"Altitude {np.min(altitude_array):.2e} is lower than LCT altitude ({lct_altitude:.2e})")
+
+        return altitude_array
+    
+
+    @staticmethod
+    def calculate_total_link_distance(sat_altitude: real_t,
+                                      lct_altitude: real_t,
+                                      zenith_angle: real_t,
+                                      **_) -> real_t:
+        """Calculate the total link distance between LCT/OGS and satellite.
+
+        Args:
+            sat_altitude (real_t): altitude to the satellite from sea level [m].
+            lct_altitude (real_t): altitude of the laser communication terminal/optical ground station above sea level [m].
+            zenith_angle (real_t): zenith angle of the link [rad].
+
+        Returns:
+            real_t: distance from the LCT/OGS to the satellite [m].
+        """
+
+        if sat_altitude < lct_altitude:
+            raise ValueError("Satellite altitude is less than LCT altitude.")
+        
+        link_distance = (sat_altitude - lct_altitude) / np.cos(zenith_angle)
+
+        return link_distance
+
+class RoundEarthModel(EarthModelGeneric):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    @staticmethod
+    def altitude_to_link[T: real_t | real_array_t](
+                         altitude_array: T,
+                         sat_altitude: real_t, 
+                         lct_altitude: real_t, 
+                         zenith_angle: real_t,
+                         **_) -> T:
+        """From an array with altitudes from sea level calculate the corresponding array with \
+            link distances (distance from the OGS to that point for each altitude). 
+
+        Args:
+            altitude_array (real_array_t): array with altitudes from lct_altitude to sat_altitude [m].
+            sat_altitude (real_t): satellite altitude [m].
+            lct_altitude (real_t): laser communication terminal/optical ground station altitude [m].
+            zenith_angle (real_t): zenith angle of the link [rad].
+
+        Returns:
+            real_array_t: array with the corresponding link distances for the given altitudes [m].
+        """
+        
+        if np.max(altitude_array) > sat_altitude:
+            raise ValueError(f"Altitude {np.max(altitude_array):.2e} is greater than the satellite altitude ({sat_altitude:.2e})")
+        
+        if np.min(altitude_array) < lct_altitude:
+            raise ValueError(f"Altitude {np.min(altitude_array):.2e} is lower than LCT altitude ({lct_altitude:.2e})")
+
+        left_term = - np.cos(zenith_angle) * ( EARTH_RADIUS + lct_altitude )
+        in_sqrt1 = np.pow( np.cos(zenith_angle) * ( EARTH_RADIUS + lct_altitude ) , 2 )
+        in_sqrt2 = (altitude_array - lct_altitude) * ( 2 * EARTH_RADIUS + altitude_array + lct_altitude)  # ty:ignore[unsupported-operator]
+
+        link_array = left_term + np.sqrt( in_sqrt1 + in_sqrt2 )
+
+        return link_array
+    
+    @staticmethod
+    def link_to_altitude[T: real_t | real_array_t](
+                         link_array: T,
+                         sat_altitude: real_t, 
+                         lct_altitude: real_t, 
+                         zenith_angle: real_t,
+                         **_) -> T:
+        """From an array with link distances calculate the corresponding array with altitudes \
+        (altitude from sea level for each corresponding link distance). 
+
+        Args:
+            link_array (real_array_t): array with link distances from the LCT to the satellite [m].
+            sat_altitude (real_t): satellite altitude [m].
+            lct_altitude (real_t): laser communication terminal/optical ground station altitude [m].
+            zenith_angle (real_t): zenith angle of the link [rad].
+
+        Returns:
+            real_array_t: array with the corresponding altitudes for the given link distances [m].
+        """
+
+        in_sqrt1 = np.pow( lct_altitude + EARTH_RADIUS, 2 )
+        in_sqrt2 = 2 * link_array * (lct_altitude + EARTH_RADIUS) * np.cos(zenith_angle)  # ty:ignore[unsupported-operator]
+        in_sqrt3 = np.pow( link_array, 2 )
+        sqrt_term = np.sqrt( in_sqrt1 + in_sqrt2 + in_sqrt3 )
+
+        altitude_array = sqrt_term - EARTH_RADIUS
+
+        if (np.max(altitude_array) > sat_altitude):
+            raise ValueError(f"Altitude {np.max(altitude_array):.2e} is greater than the satellite altitude ({sat_altitude:.2e})")
+        
+        if np.min(altitude_array) < lct_altitude:
+            raise ValueError(f"Altitude {np.min(altitude_array):.2e} is lower than LCT altitude ({lct_altitude:.2e})")
+        
+        return altitude_array
+    
+
+    @staticmethod
+    def calculate_total_link_distance(sat_altitude: real_t,
+                                      lct_altitude: real_t,
+                                      zenith_angle: real_t,
+                                      **_) -> real_t:
+        """Calculate the total link distance between LCT/OGS and satellite.
+
+        Args:
+            sat_altitude (real_t): altitude to the satellite from sea level [m].
+            lct_altitude (real_t): altitude of the laser communication terminal/optical ground station above sea level [m].
+            zenith_angle (real_t): zenith angle of the link [rad].
+
+        Returns:
+            real_t: distance from the LCT/OGS to the satellite [m].
+        """
+
+        if sat_altitude < lct_altitude:
+            raise ValueError("Satellite altitude is less than LCT altitude.")
+        
+        left_term = - np.cos(zenith_angle) * ( EARTH_RADIUS + lct_altitude )
+        in_sqrt1 = np.pow( np.cos(zenith_angle) * ( EARTH_RADIUS + lct_altitude ) , 2 )
+        in_sqrt2 = (sat_altitude - lct_altitude) * ( 2 * EARTH_RADIUS + sat_altitude + lct_altitude)
+
+        link_distance = left_term + np.sqrt( in_sqrt1 + in_sqrt2 )
+
+        return link_distance
